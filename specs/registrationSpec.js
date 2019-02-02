838e6de97db1a31382ab812f9e29193229ac3d0a
drazzilblol <paskalj10@gmail.com>
2019-02-02 17:13:44 +0300

    specs refactoring

--- specs/registrationSpec.js
+++ specs/registrationSpec.js
@@ -1,40 +1,58 @@
-describe('registration form', function () {
-    let registrationPage = require('../pageObjects/registrationPage.js');
-    let userListPage = require('../pageObjects/userListPage.js');
-    let navigator = require('../utils/navigator.js');
-    let registrationData = require('../fixtures/data.json');
+let registrationPage = require('../pageObjects/registrationPage.js');
+let userListPage = require('../pageObjects/userListPage.js');
+let navigator = require('../utils/navigator.js');
+let registrationData = require('../fixtures/data.json');
 
+describe('Check user registration form cleaning', function () {
     beforeAll(function () {
         navigator.startApp();
     });
 
-    beforeEach(function () {
-        navigator.goToIndexPage();
-    });
-
-    it('should clear registration fields', function () {
+    it('Fill Name, Address, Email fields', function () {
         registrationPage.setNameFieldText(registrationData.userData.name);
         registrationPage.setAddressFieldText(registrationData.userData.address);
         registrationPage.setEmailFieldText(registrationData.userData.email);
         expect(registrationPage.isResetButtonEnabled()).toBe(true);
+    });
+
+    it('Click on the Reset Form button', function () {
         registrationPage.resetButtonClick();
         registrationPage.isRegistrationFormCleared();
     });
+});
 
-    it('should check name and email fields validation', function () {
+describe('Check validation of email and name fields', function () {
+    beforeAll(function () {
+        navigator.goToIndexPage();
+    });
+
+    it('Enter an invalid name in the Name field that is less than three characters long', function () {
         registrationPage.setNameFieldText(registrationData.invalidName);
         expect(registrationPage.isNameValid()).toBe(false);
         registrationPage.resetButtonClick();
+
+    });
+
+    it('Enter incorrect email address in the Email field', function () {
         registrationPage.setEmailFieldText(registrationData.invalidEmail);
         expect(registrationPage.isEmailValid()).toBe(false);
         registrationPage.resetButtonClick();
     });
+});
 
-    it('should add user', function () {
+describe('Check add user', function () {
+    beforeAll(function () {
+        navigator.goToIndexPage();
+    });
+
+    it('Fill Name, Address, Email fields', function () {
         registrationPage.setNameFieldText(registrationData.userData.name);
         registrationPage.setAddressFieldText(registrationData.userData.address);
         registrationPage.setEmailFieldText(registrationData.userData.email);
         expect(registrationPage.isSubmitButtonEnabled()).toBe(true);
+    });
+
+    it('Click on the Add button', function () {
         registrationPage.submitButtonClick();
         expect(userListPage.isUserExist(registrationData.userData.name)).toBe(true);
         registrationPage.isRegistrationFormCleared();
--- specs/userListSpec.js
+++ specs/userListSpec.js
@@ -1,47 +1,71 @@
-describe('user list', function () {
-    let registrationPage = require('../pageObjects/registrationPage.js');
-    let userListPage = require('../pageObjects/userListPage.js');
-    let modalDialogs = require('../pageObjects/modalDialogs.js');
-    let navigator = require('../utils/navigator.js');
-    let registrationData = require('../fixtures/data.json');
-    let updateButtonText = 'Update';
-    let addButtonText = 'Add';
+let registrationPage = require('../pageObjects/registrationPage.js');
+let userListPage = require('../pageObjects/userListPage.js');
+let modalDialogs = require('../pageObjects/modalDialogs.js');
+let navigator = require('../utils/navigator.js');
+let registrationData = require('../fixtures/data.json');
+let updateButtonText = 'Update';
+let addButtonText = 'Add';
 
-    beforeEach(function () {
+describe('Check user editing', function () {
+    beforeAll(function () {
         navigator.goToIndexPage();
+        expect(userListPage.getUserListSize()).toBeGreaterThan(0);
     });
 
-    it('should edit user', function () {
-        expect(userListPage.getUserListSize()).toBeGreaterThan(0);
+    it('In the list of users, click the Edit button in the last user line', function () {
         userListPage.editLastUser();
         expect(registrationPage.getAddressFieldText()).toEqual(registrationData.userData.address);
         expect(registrationPage.getNameFieldText()).toEqual(registrationData.userData.name);
         expect(registrationPage.getEmailFieldText()).toEqual(registrationData.userData.email);
         expect(registrationPage.getSubmitButtonText()).toEqual(updateButtonText);
+    });
+
+    it('Fill Name, Address, Email fields', function () {
         registrationPage.setNameFieldText(registrationData.editData.name);
         registrationPage.setAddressFieldText(registrationData.editData.address);
         registrationPage.setEmailFieldText(registrationData.editData.email);
         expect(registrationPage.isSubmitButtonEnabled()).toBe(true);
+    });
+
+    it('Click on the Update button', function () {
         registrationPage.submitButtonClick();
         expect(userListPage.isUserExist(registrationData.editData.name)).toBe(true);
         registrationPage.isRegistrationFormCleared();
         expect(registrationPage.getSubmitButtonText()).toEqual(addButtonText);
     });
+});
 
-    it('should cancel remove user', function () {
+describe('Check cancel user deleting', function () {
+    beforeAll(function () {
+        navigator.goToIndexPage();
         expect(userListPage.getUserListSize()).toBeGreaterThan(0);
+    });
+
+    it('In the list of users, click the Remove button in the last user line', function () {
         userListPage.removeLastUser();
         expect(modalDialogs.isDeleteUserDialogVisible()).toBe(true);
+    });
+
+    it('Cancel deletion in the dialog by clicking the Cancel button', function () {
         modalDialogs.cancelDeleteUser();
         expect(userListPage.isUserExist(registrationData.editData.name)).toBe(true);
         expect(modalDialogs.isDeleteUserDialogVisible()).toBe(false);
     });
+});
 
-    it('should remove user', function () {
+describe('Check user deleting', function () {
+    beforeAll(function () {
+        navigator.goToIndexPage();
         expect(userListPage.getUserListSize()).toBeGreaterThan(0);
+    });
+
+    it('In the list of users, click the Remove button in the last user line', function () {
         userListPage.removeLastUser();
         expect(modalDialogs.isDeleteUserDialogVisible()).toBe(true);
         expect(modalDialogs.isDeleteDialogContainUserName(registrationData.editData.name)).toBe(true);
+    });
+
+    it('Confirm deletion in the dialog by clicking the Ok button', function () {
         modalDialogs.confirmDeleteUser();
         expect(userListPage.isUserExist(registrationData.editData.name)).toBe(false);
     });
